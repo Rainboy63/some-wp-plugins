93a67aad6b754e161beb24abee511647aebdd10a
Rainboy <olst63@mail.ru>
2019-05-31 22:30:47 +0300

    release v1

---
+++ readme.txt
@@ -0,0 +1,54 @@
+=== Translit For Search - Better Search for Inattentive Users ===
+Contributors: OlSt
+Donate link: https://...
+Tags: search, relevance, better search, translit
+Requires at least: 1.0
+Tested up to: 1.0
+Requires PHP: 5.6
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Расширение функциональности плагина поиска Relevanssi - поиск в прямой и, если ничего не найдено, автоматический поиск в обратной раскладке клавиатуры (русской или английской).
+
+== Description ==
+
+Расширение функциональности плагина поиска Relevanssi - поиск в прямой и, если ничего не найдено, в обратной раскладке клавиатуры (если термин поиска случайно введен при ошибочной раскладке, русской или английской).
+Принцип: ищем в содержимом по введенному слову, если не находим ничего, ищем по слову в обратной раскладе (русская <--> english или english <--> русская), если находим, то выводим результаты. Если не находим опять ничего, то выводим оригинальное слово и что ничего не найдено.
+Функция изменения параметров и запуска повторного поиска данного плагина прикрепляется к фильтру-хуку relevanssi_search_again плагина Relevanssi.
+
+= Key features =
+
+= Advanced features =
+
+= Premium features =
+
+= Translit For Search in Facebook =
+You can find [Translit For Search in Facebook](https://www.facebook.com/...).
+
+== Screenshots ==
+
+== Installation ==
+
+1. Install the plugin from the WordPress plugin screen.
+2. Activate the plugin.
+3. That's it!
+
+= Uninstalling =
+To uninstall the plugin remove the plugin using the normal WordPress plugin management tools (from the Plugins page, first Deactivate, then Delete).
+
+== Frequently Asked Questions ==
+
+= Knowledge Base =
+You can find solutions and answers at the [Relevanssi Knowledge Base](https://www.relevanssi.com/category/knowledge-base/).
+
+= Contextual help =
+
+= Translit For Search doesn't work =
+
+== Thanks ==
+* Dmitry Stefantsov for extensive and relentless technical aid.
+
+== Changelog ==
+= 1.0 =
+* All features and functions - new.
---
+++ translit-for-search.php
@@ -0,0 +1,51 @@
+<?php
+	/*
+	 * Plugin Name: Translit For Search
+	 * Plugin URI: http://train2019.local/
+	 * Description: Расширение функциональности плагина поиска Relevanssi - поиск в прямой и, если ничего не найдено, автоматический поиск в обратной раскладке клавиатуры (русской или английской).
+	 * Version: 1.0
+	 * Author: Олег Стефанцов
+	 * Author URI: http://plc-ok.ru/
+	 */
+	/*  Copyright 2019  OLEG_STEFANTSOV  (email: olst63@mail.ru, olst63@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+	*/
+	
+	/*Изменение раскладки строки*/
+
+	function switcher($text)
+	{
+  		$str = ['ё' => '`', 'й' => 'q', 'ц' => 'w', 'у' => 'e', 'к' => 'r', 'е' => 't', 'н' => 'y', 'г' => 'u', 'ш' => 'i', 'щ' => 'o', 'з' => 'p', 'х' => '[', 'ъ' => ']', 'ф' => 'a', 'ы' => 's', 'в' => 'd', 'а' => 'f', 'п' => 'g', 'р' => 'h', 'о' => 'j', 'л' => 'k', 'д' => 'l', 'ж' => ';', 'э' => '\'', 'я' => 'z', 'ч' => 'x', 'с' => 'c', 'м' => 'v', 'и' => 'b', 'т' => 'n', 'ь' => 'm', 'б' => ',', 'ю' => '.', '.' => '/', 'Й' => 'Q', 'Ц' => 'W', 'У' => 'E', 'К' => 'R', 'Е' => 'T', 'Н' => 'Y', 'Г' => 'U', 'Ш' => 'I', 'Щ' => 'O', 'З' => 'P', 'Х' => '{', 'Ъ' => '}', 'Ф' => 'A', 'Ы' => 'S', 'В' => 'D', 'А' => 'F', 'П' => 'G', 'Р' => 'H', 'О' => 'J', 'Л' => 'K', 'Д' => 'L', 'Ж' => ':', 'Э' => '"', 'Я' => 'Z', 'Ч' => 'X', 'С' => 'C', 'М' => 'V', 'И' => 'B', 'Т' => 'N', 'Ь' => 'M', 'Б' => '<', 'Ю' => '>', ',' => '?', '`' => 'ё', 'q' => 'й', 'w' => 'ц', 'e' => 'у', 'r' => 'к', 't' => 'е', 'y' => 'н', 'u' => 'г', 'i' => 'ш', 'o' => 'щ', 'p' => 'з', '[' => 'х', ']' => 'ъ', 'a' => 'ф', 's' => 'ы', 'd' => 'в', 'f' => 'а', 'g' => 'п', 'h' => 'р', 'j' => 'о', 'k' => 'л', 'l' => 'д', ';' => 'ж', '\'' => 'э', 'z' => 'я', 'x' => 'ч', 'c' => 'с', 'v' => 'м', 'b' => 'и', 'n' => 'т', 'm' => 'ь', ',' => 'б', '.' => 'ю','/' => '.', 'Q' => 'Й', 'W' => 'Ц', 'E' => 'У', 'R' => 'К', 'T' => 'Е', 'Y' => 'Н', 'U' => 'Г', 'I' => 'Ш', 'O' => 'Щ', 'P' => 'З', '{' => 'Х', '}' => 'Ъ', 'A' => 'Ф', 'S' => 'Ы', 'D' => 'В', 'F' => 'А', 'G' => 'П', 'H' => 'Р', 'J' => 'О', 'K' => 'Л', 'L' => 'Д', ':' => 'Ж', '"' => 'Э', 'Z' => 'Я', 'X' => 'Ч', 'C' => 'С', 'V' => 'М', 'B' => 'И', 'N' => 'Т', 'M' => 'Ь', '<' => 'Б', '>' => 'Ю', '?' => ','];
+  		return strtr($text, $str);
+	}
+
+	/*Функция изменения параметров и запуска повторного поиска, прикрепляемая к фильтру-хуку relevanssi_search_again плагина Relevanssi*/
+
+	function start_search_again($params) { 
+	    if ($params['no_matches'] == true) 
+	    	{
+	    		$params['search_again'] = true;
+	    		foreach ($params['terms'] as $k => $v) {
+	    			$params['terms'][$k] = switcher($v);
+	    		}
+	    		$params['no_matches'] = false;
+	    	}
+	    return $params; 
+	}
+
+	add_filter('relevanssi_search_again', 'start_search_again');
+	//add_filter('relevanssi_query_filter', 'edit_query_filter');
+	//add_filter('relevanssi_search_filters', 'edit_search_filters');
